50c70bd7fd2a402db0ae75fa0a125f10f05b9c15
ykxtais <nyxtais@gmail.com>
2025-11-22 17:55:52 -0300

    texto para não quebrar

---
+++ src/screens/Trilhas.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { auth } from '../firebase/firebaseConfig';
+import { useThemedStyles } from '../context/themeContext';
+
+export default function Trilhas() {
+
+  return (
+    <Text>Trilhas</Text>
+    );
+}
